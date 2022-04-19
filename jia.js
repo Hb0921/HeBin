dd1e2eae5e9e6c5a443bf9b5b76660ff0f226f80
Hb0921 <103397918+Hb0921@users.noreply.github.com>
2022-04-19 17:23:40 +0800

    Add files via upload

---
+++ jia.js
@@ -0,0 +1,11 @@
+/*BinHe Michael
+1.Interpretive scripting languages
+2.Interpretive scripting languages
+3.Is a secure language
+*/
+function jiajian(){
+    var a=document.getElementById('a').value;
+    var b=document.getElementById('b').value;
+    var c=document.getElementById('c');
+    c.value=Number(a)+Number(b);
+}
